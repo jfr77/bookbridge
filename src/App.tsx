f1c5a7833b3eb84157b88cc3cdc14dda2e3f049f
vicky <vicky.yiran.li@gmail.com>
2026-01-10 11:16:52 +0100

    4

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Landing from "./Landing.tsx"
+import Landing from "./components/Landing.tsx"
 
 
 function App() {
